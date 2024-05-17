104cb67b536298dc839e7a314998686877dff00e
Abdulrahman-Alaa7 <wl3elkobs@gmail.com>
2024-05-17 23:40:33 +0300

    update

--- redux/features/api/apiSlice.ts
+++ redux/features/api/apiSlice.ts
@@ -4,7 +4,8 @@ import { userLoggedIn } from "../auth/authSlice";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_SERVER_URL,
+    baseUrl:
+      "https://rockies-server-beta-qgy039own-abdulrahmanalaa7s-projects.vercel.app/api/v1/",
   }),
   endpoints: (builder) => ({
     refreshToken: builder.query({
